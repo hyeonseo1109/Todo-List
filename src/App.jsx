537bd43931e355b3fba9da498be4c65ede9f4b16
hyeonseo1109 <alsgustj2@naver.com>
2025-07-08 16:22:20 +0900

    주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import './App.css';
 
 function App() {
   const [isLoading, data] = useFetch("http://localhost:3000/todo");
+  //json server에서 받아옴
   const [todo, setTodo] = useState([]);
   const [currentTodo, setCurrentTodo] = useState(null);
   const [time, setTime] = useState(0)
   const [isTimer, setIsTimer] = useState(false);
 
   useEffect( () => {
-    if (currentTodo) {  //currentTodo가 있으면
+    if (currentTodo) {  
+      //currentTodo가 있으면
       const current = todo.find( (el) => el.id === currentTodo);
+      //현재 선택한 아이디랑 같은 아이디를 찾아서 걔를 current라고 할 거다.
       if (!current) {
         setCurrentTodo(null);
         return;
@@ -20,7 +23,7 @@ function App() {
         //GET: 데이터 조회, POST: 데이터 생성, PUT: 전체 덮어쓰기, PATCH: 일부 필드만 수정, DELETE: 데이터 삭제
         body: JSON.stringify({ 
           time: todo.find((el) => el.id === currentTodo).time + 1
-          //리스트별 타이머
+          //리스트별 타이머. 선택된 애의 time속성을 1씩 늘림
         }),
       })
       .then( (res) => res.json())
@@ -35,6 +38,7 @@ function App() {
   useEffect( () => {
     setTime(0);
   }, [isTimer]);
+  //타이머로 변경될 때마다 0초로 만듦
   
   useEffect( () => {
     if (data) {
@@ -81,13 +85,6 @@ const Advice = () => {
     <>
       {!isLoading && (
         <>
-        {/* <p>˘꒷꒦˘꒷꒦꒷˘꒦꒷꒦˘꒦˘꒷꒦꒷˘꒦꒷꒦˘꒷꒦˘꒷꒦꒷˘꒦꒷꒦˘꒦˘꒷꒦꒷˘꒦꒷꒦˘</p>
-        <p>∘ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-          ∘ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   
-          ° &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
-          ∘ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-          ∘ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-        </p> */}
         {/* <p>
           𓏧 𓏲 𓏲 𓏲 𓋒𓏲 𓏲 𓏲 𓏲 𓏧 𓏲 𓏲 𓏲 𓋒𓏲 𓏲 𓏲 𓏲 𓏧 𓏲 𓏲 𓏲 𓋒𓏲 𓏲 𓏲 𓏲 𓏧
         </p>
@@ -112,14 +109,18 @@ const Advice = () => {
 const Timer = ({ time, setTime }) => {
   const [startTime, setStartTime] = useState(0)
   const [isOn, setIsOn] = useState(false)
+  //우선 처음에는 꺼놓은 상태로 시작
   const timerRef = useRef(null)
 
   useEffect( () => {
     if (isOn && time >0) {
+      //타이머가 켜져있고 시간이 아직 남아있으면?
       const timerId = setInterval( () => {
         setTime( (prev) => prev -1)
       }, 1000);
+      //1초마다 감소되게끔.
       timerRef.current = timerId
+      //값이 바뀌어도 리렌더링 되지 않는 ref 이용, 타이머 아이디를 저장하고 clear함
     } else if (!isOn || time === 0) {
       clearInterval(timerRef.current);
     }
@@ -132,14 +133,16 @@ const Timer = ({ time, setTime }) => {
           className="swCss"
           type="range" 
           value={startTime} 
+          //startTime값이랑 똑같게 연결해줌
           min="0"
           max='2400'
           step="5"
           onChange={ (event) => setStartTime(event.target.value) } 
         />
-
+  
       <div className='swFormat'>
-        {time ? formatTime(time) : formatTime(startTime)}
+        {time ? formatTime(time) : formatTime(startTime)} 
+        {/*스톱워치에 이미 지정된 시간이 있으면 그걸 포맷화해서 띄우고 아님 시작시간 띄우기*/}
         <div className="swBtCss">
           <button onClick={ () => {
             setIsOn(true);
@@ -171,8 +174,10 @@ const Clock = () => {
   useEffect( () => {
     setInterval( () => {
       setTime(new Date())
+      //1초에 한번씩 time을 new Date로 불러와줌
     }, 1000)
   }, [])
+  //처음 한 번 마운트될 떄만 렌더링되지만 인터벌이 1초마다 시간 바꿔줌
 
   return (
     <div className='clock'>
@@ -182,7 +187,7 @@ const Clock = () => {
 }
 
 const formatTime = (seconds) => {
-  // 12345초가 있으면s
+  // 12345초가 있으면
   // 12345/3600 (절대값) -> 시간
   // (12345 % 3600) / 60 (절대값) -> 분
   // 12345 % 60 -> 초
@@ -209,7 +214,7 @@ const StopWatch = ({ time, setTime }) => {
     <div className="tmFormat">
       {formatTime(time)}
       <div>
-        <button onClick={ () => setIsOn( (prev) => !prev )}>{isOn ? "끄기" : "켜기"}</button>
+        <button onClick={ () => setIsOn( (prev) => !prev )}>{isOn ? "끄기" : "켜기"}</button> {/*토글.. 누르면 반대의 상태가 됨*/}
         <button onClick={() => {
           setTime(0);
           setIsOn(false);
@@ -226,11 +231,10 @@ const StopWatch = ({ time, setTime }) => {
 
 
 
-
 const TodoList = ( {todo, setTodo, setCurrentTodo, currentTodo }) => {
   return (
       <ul>
-        {todo.map( (el) => (
+        {todo.map( (el) => (  //투두를 받아와서 map으로 .. 컴포넌트 하나씩 생성해서 뿌림
           <Todo key={todo.id} todo={el} setTodo={setTodo} setCurrentTodo={setCurrentTodo} currentTodo={currentTodo}/>
         ))}
       </ul>
@@ -241,8 +245,10 @@ const Todo = ({ todo, setTodo, setCurrentTodo, currentTodo }) => {
   return (
     <>
       <li className={currentTodo === todo.id ? 'current list' : "list"}>
+        {/*지금 선택된 애는 따로 클래스 더 지정해줌*/}
         <div>
-          {todo.content}
+          {todo.content}  
+            {/*TodoList에서 map으로 뿌려준 하나하나의 리스트를 보여줌 */}
         </div>
         <div className='listBts'>
           {formatTime(todo.time)}
